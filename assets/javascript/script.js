046a4d6b9387252863b0016fffed3851fd2697ed
copiousamounts <nick.hernstedt@gmail.com>
2020-02-20 11:31:48 -0800

    added dom elements and got search function working

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -4,3 +4,50 @@ const random = document.getElementById("random");
 const mealsEl = document.getElementById("meals");
 const resultHeading = document.getElementById("result-heading");
 const single_mealEl = document.getElementById("single-meal");
+
+// Search meal and fetch from API
+function searchMeal(e) {
+  e.preventDefault();
+
+  // clear single meal
+  single_mealEl.innerHTML = "";
+
+  // Get search term
+  const term = search.value;
+
+  // check for empty
+  if (term.trim()) {
+    fetch(`https://www.themealdb.com/api/json/v1/1/search.php?s=${term}`)
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+        resultHeading.innerHTML = `<h2>Search results for '${term}':</h2>`;
+
+        if (data.meals == null) {
+          resultHeading.innerHTML =
+            "<p>There are no search results try again</p>";
+        } else {
+          mealsEl.innerHTML = data.meals
+            .map(
+              meal => `
+            <div class="meal"> 
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" />
+            <div class="meal-info" data-mealID="${meal.idMeal}">
+                <h3>${meal.strMeal}</h3>
+              </div>
+            </div>
+            `
+            )
+            .join("");
+        }
+      });
+
+    //   clear search text
+    search.value = "";
+  } else {
+    alert("Please enter a search term");
+  }
+}
+
+//  Event listeners
+submit.addEventListener("submit", searchMeal);
